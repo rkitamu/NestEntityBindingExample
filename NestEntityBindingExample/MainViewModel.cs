0e4f0676d1cc304c66e773ca176b6b067a5b82b1
rkitamu <rkitamun@gmail.com>
2025-09-18 06:23:35 +0900

    multiple

--- NestEntityBindingExample/MainViewModel.cs
+++ NestEntityBindingExample/MainViewModel.cs
@@ -1,22 +1,43 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace NestEntityBindingExample
 {
     public partial class MainViewModel : ObservableObject
     {
         [ObservableProperty]
-        private UserViewModel _currentUser;
+        private ObservableCollection<UserViewModel> _users;
+
+        [ObservableProperty]
+        private UserViewModel? _selectedUser;
 
         public MainViewModel()
         {
-            var userModel = new User
+            var userModels = new[]
             {
-                Id = 101,
-                Name = "Taro Suzuki",
-                Description = "He is a software engineer."
+                new User
+                {
+                    Id = 101,
+                    Name = "Taro Suzuki",
+                    Description = "He is a software engineer."
+                },
+                new User
+                {
+                    Id = 102,
+                    Name = "Hanako Yamada",
+                    Description = "She is a designer."
+                },
+                new User
+                {
+                    Id = 103,
+                    Name = "Jiro Sato",
+                    Description = "He is a project manager."
+                }
             };
 
-            _currentUser = new UserViewModel(userModel);
+            _users = new ObservableCollection<UserViewModel>(userModels.Select(u => new UserViewModel(u)));
+            _selectedUser = _users.FirstOrDefault();
         }
     }
 }
